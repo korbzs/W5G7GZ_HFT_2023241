9cf4856f36756ff7074c5f4bebe0658aca251aab
Zsombor <korbzsombor@gmail.com>
2023-10-03 21:21:47 +0200

    Add project files.

---
+++ W5G7GZ_HFT_2023241.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace W5G7GZ_HFT_2023241.Repository
+{
+    public class Class1
+    {
+    }
+}
---
+++ W5G7GZ_HFT_2023241/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace W5G7GZ_HFT_2023241
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
