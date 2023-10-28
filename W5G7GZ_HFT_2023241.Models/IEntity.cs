47e2f376e0c327d10412d1b43a4e0d8af89ec897
Zsombor <korbzsombor@gmail.com>
2023-10-28 14:25:13 +0200

    Classes in Models are created, might modify later.

---
+++ W5G7GZ_HFT_2023241.Models/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W5G7GZ_HFT_2023241.Models
+{
+    public interface IEntity<TId>
+    {
+        TId Id { get; set; }
+    }
+}
