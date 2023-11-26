e8b565c059523132c92f63f9965629226ce95fcd
korbzs <korb.zsombor@stud.uni-obuda.hu>
2023-11-26 13:29:48 +0100

    booklogic

--- W5G7GZ_HFT_2023241.Logic/Logic/BookLogic.cs
+++ W5G7GZ_HFT_2023241.Logic/Logic/BookLogic.cs
@@ -13,11 +13,15 @@ namespace W5G7GZ_HFT_2023241.Logic.Logic
     public class BookLogic : IBookLogic
     {
 
-        IRepository<Book> repo;
+        IRepository<Author> AuthorRepo;
+        IRepository<Book> BookRepo;
+        IRepository<Publisher> PublisherRepo;
 
-        public BookLogic(IRepository<Book> repo)
+        public BookLogic(IRepository<Book> BookRepo, IRepository<Author> AuthorRepo, IRepository<Publisher> PublisherRepo)
         {
-            this.repo = repo;
+            this.AuthorRepo = AuthorRepo;
+            this.BookRepo = BookRepo;
+            this.PublisherRepo = PublisherRepo;
         }
 
         public void Create(Book item)
@@ -26,25 +30,45 @@ namespace W5G7GZ_HFT_2023241.Logic.Logic
             {
                 throw new ArgumentException("Book name is too short...");
             }
-            this.repo.Create(item);
+            this.BookRepo.Create(item);
         }
         public Book Read(int id)
         {
-            return this.repo.Read(id);
+            return this.BookRepo.Read(id);
         }
         public void Update(Book item)
         {
-            this.repo.Update(item);
+            this.BookRepo.Update(item);
         }
 
         public void Delete(int id)
         {
-            this.repo.Delete(id);
+            this.BookRepo.Delete(id);
         }
 
         public IQueryable<Book> ReadAll()
         {
-            return this.repo.ReadAll();
+            return this.BookRepo.ReadAll();
+        }
+
+        public IEnumerable<KeyValuePair<string, int>> BookCountPerPublisher()
+        {
+            var bookCounts = BookRepo.ReadAll()
+        .GroupBy(b => b.Publisher.PublisherName)
+        .Select(group => new KeyValuePair<string, int>(group.Key, group.Count()));
+
+            return bookCounts;
+        }
+
+        public KeyValuePair<string, int> AuthorWithTheMostBooks()
+        {
+            var authorWithMostBooks = BookRepo.ReadAll()
+                .GroupBy(b => b.Author.AuthorName)
+                .Select(group => new KeyValuePair<string, int>(group.Key, group.Count()))
+                .OrderByDescending(pair => pair.Value)
+                .FirstOrDefault();
+
+            return authorWithMostBooks;
         }
 
     }
